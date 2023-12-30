4075e558aa35f8479d1b8ab49349ff4a7d3e7231
John Smith <john.smith@example.xyz>
2023-12-30 08:02:07 +0100

    Refactor BerlinClock

--- BerlinClock/Model/BerlinClock.swift
+++ BerlinClock/Model/BerlinClock.swift
@@ -14,40 +14,9 @@ struct BerlinClock {
     
     init(date: Date, calendar: Calendar) {
         secondHighlighted = calendar.component(.second, from: date) % 2 == 0
-
-        let hourComponentValue = calendar.component(.hour, from: date)
-        let minuteComponentValue = calendar.component(.minute, from: date)
-
-        let numberOfFiveHourActiveBlocks = hourComponentValue / 5
-        let fiveHourBlockArrayCapacity = 4
-        let numberOfFiveHourInactiveBlocks = fiveHourBlockArrayCapacity - numberOfFiveHourActiveBlocks
-        let activeFiveHourBlocks = (0..<numberOfFiveHourActiveBlocks).map { _ in true }
-        let inactiveFiveHourBlocks = (0..<numberOfFiveHourInactiveBlocks).map { _ in false }
-        
-        fiveHourBlocks = activeFiveHourBlocks + inactiveFiveHourBlocks
-        
-        let numberOfOneHourActiveBlocks = hourComponentValue % 5
-        let oneHourBlockArrayCapacity = 4
-        let numberOfOneHourInactiveBlocks = oneHourBlockArrayCapacity - numberOfOneHourActiveBlocks
-        let activeOneHourBlocks = (0..<numberOfOneHourActiveBlocks).map { _ in true }
-        let inactiveOneHourBlocks = (0..<numberOfOneHourInactiveBlocks).map { _ in false }
-
-        oneHourBlocks = activeOneHourBlocks + inactiveOneHourBlocks
-
-        let numberOfFiveMinuteActiveBlocks = minuteComponentValue / 5
-        let fiveMinuteBlockArrayCapacity = 11
-        let numberOfFiveMinuteInactiveBlocks = fiveMinuteBlockArrayCapacity - numberOfFiveMinuteActiveBlocks
-        let activeFiveMinuteBlocks = (0..<numberOfFiveMinuteActiveBlocks).map { _ in true }
-        let inactiveFiveMinuteBlocks = (0..<numberOfFiveMinuteInactiveBlocks).map { _ in false }
-
-        fiveMinuteBlocks = activeFiveMinuteBlocks + inactiveFiveMinuteBlocks
-        
-        let numberOfOneMinuteActiveBlocks = minuteComponentValue % 5
-        let oneMinuteBlockArrayCapacity = 4
-        let numberOfOneMinuteInactiveBlocks = oneMinuteBlockArrayCapacity - numberOfOneMinuteActiveBlocks
-        let activeOneMinuteBlocks = (0..<numberOfOneMinuteActiveBlocks).map { _ in true }
-        let inactiveOneMinuteBlocks = (0..<numberOfOneMinuteInactiveBlocks).map { _ in false }
-
-        oneMinuteBlocks = activeOneMinuteBlocks + inactiveOneMinuteBlocks
+        fiveHourBlocks = BlockUnit.fiveHour.blockRepresentation(date: date, calendar: calendar)
+        oneHourBlocks = BlockUnit.oneHour.blockRepresentation(date: date, calendar: calendar)
+        fiveMinuteBlocks = BlockUnit.fiveMinute.blockRepresentation(date: date, calendar: calendar)
+        oneMinuteBlocks = BlockUnit.oneMinute.blockRepresentation(date: date, calendar: calendar)
     }
 }
---
+++ BerlinClock/Model/BlockUnit.swift
@@ -0,0 +1,85 @@
+//
+//  BlockUnit.swift
+//  BerlinClock
+//
+
+import Foundation
+
+enum BlockUnit {
+
+    /// The first row of a Berlin Clock exists out of 4 five hour blocks.
+    case fiveHour
+
+    /// The second row of a Berlin Clock exists out of 4 one hour blocks.
+    case oneHour
+
+    /// The third row of a Berlin Clock exists out of 11 five minute blocks.
+    case fiveMinute
+
+    /// The first row of a Berlin Clock exists out of 4 one minute blocks.
+    case oneMinute
+
+}
+
+extension BlockUnit {
+    
+    private var value: Int {
+        switch self {
+        case .fiveHour, .fiveMinute:
+            return 5
+        case .oneHour, .oneMinute:
+            return 1
+        }
+    }
+    
+    private var arrayCount: Int {
+        switch self {
+        case .fiveHour:
+            return 4
+        case .oneHour:
+            return 4
+        case .fiveMinute:
+            return 11
+        case .oneMinute:
+            return 4
+        }
+    }
+    
+    private var calendarComponent: Calendar.Component {
+        switch self {
+        case .fiveHour, .oneHour:
+            return .hour
+        case .fiveMinute, .oneMinute:
+            return .minute
+        }
+    }
+    
+    private var procedingBlockUnit: BlockUnit? {
+        switch self {
+        case .fiveHour, .fiveMinute:
+            return nil
+        case .oneHour:
+            return .fiveHour
+        case .oneMinute:
+            return .fiveMinute
+        }
+    }
+    
+    func blockRepresentation(date: Date, calendar: Calendar) -> [Bool] {
+        let componentValue = calendar.component(self.calendarComponent, from: date)
+        let numberOfActiveBlocks = self.numberOfActiveBlocks(componentValue: componentValue)
+        let numberOfInactiveBlocks = self.arrayCount - numberOfActiveBlocks
+        let activeBlocks = (0..<numberOfActiveBlocks).map { _ in true }
+        let inactiveBlocks = (0..<numberOfInactiveBlocks).map { _ in false }
+        
+        return activeBlocks + inactiveBlocks
+    }
+    
+    private func numberOfActiveBlocks(componentValue: Int) -> Int {
+        if let procedingBlockUnit = self.procedingBlockUnit {
+            return componentValue % procedingBlockUnit.value
+        } else {
+            return componentValue / self.value
+        }
+    }
+}
