198f9f8d61d86f98c49303187e9c3f1b37a0a7b4
John Smith <john.smith@example.xyz>
2023-12-30 08:22:20 +0100

    Add BerlinClockViewModelTests

---
+++ BerlinClockTests/UI/UI/BerlinClockViewModelTests.swift
@@ -0,0 +1,14 @@
+//
+//  BerlinClockViewModelTests.swift
+//  BerlinClockTests
+//
+
+import Foundation
+import XCTest
+import SwiftUI
+@testable import BerlinClock
+
+class BerlinClockViewModelTests: XCTestCase {
+    
+
+}
