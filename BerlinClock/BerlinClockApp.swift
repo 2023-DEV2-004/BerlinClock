fb8d91adb90ac7e34ea3c70da7982950231bdac1
John Smith <john.smith@example.xyz>
2023-12-30 06:25:23 +0100

    Initial commit

---
+++ BerlinClock/BerlinClockApp.swift
@@ -0,0 +1,15 @@
+//
+//  BerlinClockApp.swift
+//  BerlinClock
+//
+
+import SwiftUI
+
+@main
+struct BerlinClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
