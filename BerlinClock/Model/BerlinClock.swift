928234fad9e8e68c2e52ae65edfdec95069cc246
John Smith <john.smith@example.xyz>
2023-12-30 06:51:14 +0100

    Make array capacity test pass

--- BerlinClock/Model/BerlinClock.swift
+++ BerlinClock/Model/BerlinClock.swift
@@ -6,8 +6,15 @@
 import Foundation
 
 struct BerlinClock {
-    let fiveHourBlocks: [Bool] = []
-    let oneHourBlocks: [Bool] = []
-    let fiveMinuteBlocks: [Bool] = []
-    let oneMinuteBlocks: [Bool] = []
+    let fiveHourBlocks: [Bool]
+    let oneHourBlocks: [Bool]
+    let fiveMinuteBlocks: [Bool]
+    let oneMinuteBlocks: [Bool]
+    
+    init() {
+        fiveHourBlocks = Array(repeating: false, count: 4)
+        oneHourBlocks = Array(repeating: false, count: 4)
+        fiveMinuteBlocks = Array(repeating: false, count: 11)
+        oneMinuteBlocks = Array(repeating: false, count: 4)
+    }
 }
