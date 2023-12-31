2e42a834fde8f183ccb77aabdecbd059cb0a6e72
John Smith <john.smith@example.xyz>
2023-12-31 11:26:58 +0100

    Add UI-Test

--- BerlinClock/UI/BerlinClockView.swift
+++ BerlinClock/UI/BerlinClockView.swift
@@ -14,23 +14,38 @@ struct BerlinClockView: View {
         static let padding: CGFloat = 5
         static let sectionHeight: CGFloat = 100
     }
+    
+    private struct Accessibility {
+        static let secondHighlightIdentifier = "secondHighlightIdentifier"
+        static let fiveHourBlockSectionIdentifier = "fiveHourBlockSectionIdentifier"
+        static let oneHourBlockSectionIdentifier = "oneHourBlockSectionIdentifier"
+        static let fiveMinuteBlockSectionIdentifier = "fiveMinuteBlockSectionIdentifier"
+        static let oneMinuteBlockSectionIdentifier = "oneMinuteBlockSectionIdentifier"
+        static let timeDescriptionIdentifier = "timeDescriptionIdentifier"
+    }
 
     var body: some View {
         VStack {
             SecondSectionView(color: self.viewModel.secondHighlightColor)
                 .frame(height: Constants.sectionHeight)
+                .accessibilityIdentifier(Accessibility.secondHighlightIdentifier)
             BlockSectionView(blocks: self.viewModel.fiveHourBlocks)
                 .frame(height: Constants.sectionHeight)
+                .accessibilityIdentifier(Accessibility.fiveHourBlockSectionIdentifier)
             BlockSectionView(blocks: self.viewModel.oneHourBlocks)
                 .frame(height: Constants.sectionHeight)
+                .accessibilityIdentifier(Accessibility.oneHourBlockSectionIdentifier)
             BlockSectionView(blocks: self.viewModel.fiveMinuteBlocks)
                 .frame(height: Constants.sectionHeight)
+                .accessibilityIdentifier(Accessibility.fiveMinuteBlockSectionIdentifier)
             BlockSectionView(blocks: self.viewModel.oneMinuteBlocks)
                 .frame(height: Constants.sectionHeight)
+                .accessibilityIdentifier(Accessibility.oneMinuteBlockSectionIdentifier)
             
             Spacer()
             
             TimeDescriptionView(time: self.viewModel.timeDescription)
+                .accessibilityIdentifier(Accessibility.timeDescriptionIdentifier)
             
             Spacer()
         }
